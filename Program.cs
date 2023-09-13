e040c0d9978d7b6f8fdd1814104f996eac5bb341
Андрей Мисирук <therappy13@ya.ru>
2023-09-13 23:41:11 +0300

    Успешное выполнение 8.3% ms, 5.9 mb

--- Program.cs
+++ Program.cs
@@ -1,41 +1,7 @@
-﻿
-var res = NumIslands(new char[][] {
+﻿using leetCode.questions;
+using leetCode.questions;
+using leetCode.questions;
+using leetCode.questions;
 
- new char[] {'1', '1', '1', '1', '0' },
- new char[] {'1', '1', '0', '1', '0' },
- new char[] {'1', '1', '0', '0', '0' },
- new char[] {'0', '0', '0', '0', '0' }
- });
-var qq = res;
-int NumIslands(char[][] grid)
-{
-    var count = 0;
-    for (int i = 0; i < grid.Length; i++)
-    {
-        for (int y = 0; y < grid[i].Length; y++)
-        {
-            if (grid[i][y] == '0')
-                continue;
-
-            if (i == 0)
-            {
-                if (y == 0)
-                {
-                    if (grid[i + 1][y] == '0' && grid[i][y + 1] == '0')
-                        count++;
-                    
-                    continue;
-                }
-                if (grid[i + 1][y] == '0' && grid[i][y - 1] == '0' && grid[i][y + 1] == '0')
-                    count++;
-
-                continue;
-            }
-            else if (y == 0)
-            {
-                if (grid[i - 1][y] == '0' && grid[i + 1][y] == '0' && grid[i][y + 1])
-            }
-        }
-    }
-    return count;
-}
+var longPalindSub = LongestPalindromicSubstring.LongestPalindrome("anugnxshgonmqydttcvmtsoaprxnhpmpovdolbidqiyqubirkvhwppcdyeouvgedccipsvnobrccbndzjdbgxkzdbcjsjjovnhpnbkurxqfupiprpbiwqdnwaqvjbqoaqzkqgdxkfczdkznqxvupdmnyiidqpnbvgjraszbvvztpapxmomnghfaywkzlrupvjpcvascgvstqmvuveiiixjmdofdwyvhgkydrnfuojhzulhobyhtsxmcovwmamjwljioevhafdlpjpmqstguqhrhvsdvinphejfbdvrvabthpyyphyqharjvzriosrdnwmaxtgriivdqlmugtagvsoylqfwhjpmjxcysfujdvcqovxabjdbvyvembfpahvyoybdhweikcgnzrdqlzusgoobysfmlzifwjzlazuepimhbgkrfimmemhayxeqxynewcnynmgyjcwrpqnayvxoebgyjusppfpsfeonfwnbsdonucaipoafavmlrrlplnnbsaghbawooabsjndqnvruuwvllpvvhuepmqtprgktnwxmflmmbifbbsfthbeafseqrgwnwjxkkcqgbucwusjdipxuekanzwimuizqynaxrvicyzjhulqjshtsqswehnozehmbsdmacciflcgsrlyhjukpvosptmsjfteoimtewkrivdllqiotvtrubgkfcacvgqzxjmhmmqlikrtfrurltgtcreafcgisjpvasiwmhcofqkcteudgjoqqmtucnwcocsoiqtfuoazxdayricnmwcg");
+var q = "";
---
+++ questions/LongestPalindromicSubstring.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode.questions
+{
+    internal class LongestPalindromicSubstring
+    {
+        /*Задача:
+        Given a string s, return the longest palindromic substring in s.
+        */
+        public static string LongestPalindrome(string s)
+        {
+            if (s.Length == 1)
+                return s;
+
+            string result = "";
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (result.Length > s.Length - i)
+                    return result;
+                for (int j = s.Length; j > i; j--)
+                {
+                    if (s[j - 1] != s[i])
+                        continue;
+                    else if (j - i <= result.Length)
+                        break;
+                    var text = s.Substring(i).Remove(j - i).ToString();
+                    var textRevers = new String(text.Reverse().ToArray());
+                    if (text == textRevers && text.Length > result.Length)
+                        result = text;
+                }
+            }
+            return result;
+        }
+    }
+}
